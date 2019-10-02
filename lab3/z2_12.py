bda77f58fcbb56234084a0a637bc30a7f398cc52
DrIlya <55554622+DrIlya@users.noreply.github.com>
2019-10-03 01:20:52 +0300

    Lama lama lama... More lamas!!!

--- lab3/z2_12.py
+++ lab3/z2_12.py
@@ -1,66 +1,71 @@
 from graph import *
 
 
-brushColor(68,232,76)
+brushColor(171, 223, 136)
 polygon([[0,0],[500,0],[500,600],[0,600]])
 brushColor(42,209,209)
 polygon([[0,0],[0,250],[100,240],[150,260],[250,250],[255,275],[250,300],[260,325],[250,350],[340,360],[350,340],[400,350],[500,350],[500,600],[500,0]])
-brushColor("gray")
+brushColor(180, 180, 181)
 polygon([[0,0],[0,250],[100,240],[150,260],[250,250],[255,275],[250,300],[260,325],[250,350],[340,360],[350,340],[400,350],[500,350],[400,200],[360,175],[330,200],[270,150],[200,220],[130,175],[60,75]])
-brushColor(18,163,59)
-circle(400,500,75)
+brushColor(114, 201, 52)
+penColor(114, 201, 52)
 c=canvas()
-
-def flower(x1,y1,x2,y2):
-    c.create_oval(x1, y1, x2, y2, fill="yellow")
-    c.create_oval(x1 + 40, y1+5, x2 + 20, y2, fill="white")
-    c.create_oval(x1 + 30, y1 + 15, x2 + 10, y2 + 10, fill="white")
-    c.create_oval(x1 + 20, y1+20, x2, y2+15, fill="white")
-    c.create_oval(x1 + 10, y1+20, x2-10, y2+15, fill="white")
-    c.create_oval(x1, y1+15, x2-20, y2+10, fill="white")
-    c.create_oval(x1-10,y1+5,x2-30,y2,fill="white")
-    c.create_oval(x1, y1-10, x2 - 20, y2-15, fill="white")
-    c.create_oval(x1+10, y1-15, x2-10, y2-20, fill="white")
-    c.create_oval(x1+20, y1-15, x2, y2-20, fill="white")
-    c.create_oval(x1+30, y1-10, x2 +10, y2 -15, fill="white")
-    c.create_oval(x1+35, y1-5, x2 + 15, y2 -10, fill="white")
-
-
-
-flower(410,510,460,535)
-flower(330,490,380,515)
-flower(380,440,430,465)
+def clumba(X1,Y1,s):
+    circle((3*X1 - 10*s) / 3, (Y1*3 + 80*s) / 3, 75*s)
+    def flower(x1,y1,x2,y2):
+        c.create_oval(x1, y1, x2, y2, fill="yellow")
+        c.create_oval(x1 + 40*s, y1+5*s, x2 + 20*s, y2, fill="white")
+        c.create_oval(x1 + 30*s, y1 + 15*s, x2 + 10*s, y2 + 10*s, fill="white")
+        c.create_oval(x1 + 20*s, y1+20*s, x2, y2+15*s, fill="white")
+        c.create_oval(x1 + 10*s, y1+20*s, x2-10*s, y2+15*s, fill="white")
+        c.create_oval(x1, y1+15*s, x2-20*s, y2+10*s, fill="white")
+        c.create_oval(x1-10*s,y1+5*s,x2-30*s,y2,fill="white")
+        c.create_oval(x1, y1-10*s, x2 - 20*s, y2-15*s, fill="white")
+        c.create_oval(x1+10*s, y1-15*s, x2-10*s, y2-20*s, fill="white")
+        c.create_oval(x1+20*s, y1-15*s, x2, y2-20*s, fill="white")
+        c.create_oval(x1+30*s, y1-10*s, x2 +10*s, y2 -15*s, fill="white")
+        c.create_oval(x1+35*s, y1-5*s, x2 + 15*s, y2 -10*s, fill="white")
+    flower(X1-25*s,Y1-7.5*s,X1+25*s,Y1+7.5*s)
+    flower(X1+5*s,Y1+38*s,X1+55*s,Y1+62*s)
+    flower(X1-65*s,Y1+18*s,X1-15*s,Y1+42*s)
 
 
-def legs(x,y):
-    c.create_oval(x, y, x + 30, y + 20, fill="white")
-    c.create_oval(x, y, x + 20, y - 60, fill="white")
-    c.create_oval(x, y - 45, x + 20, y - 105, fill="white")
-def lama(x, y):
+clumba(400,480,0.5)
+clumba(490,440,0.5)
+clumba(470,360,0.4)
+clumba(400,390,0.3)
+clumba(490,580,0.4)
+clumba(30,300,0.3)
+
+
+# s-относительный параметр рамера,o-отвечает за зеркальное отражение
+def lama(x, y, s, o):
+    def legs(x, y):
+        c.create_oval(x, y, x + 15*s*o, y + 10*s, fill="white")
+        c.create_oval(x, y, x + 10*s*o, y - 30*s, fill="white")
+        c.create_oval(x, y - 21*s, x + 10*s*o, y - 52*s, fill="white")
     legs(x,y)
-    legs(x-75,y)
-    legs(x-110,y-30)
-    legs(x-35,y-30)
-    c.create_oval(x + 20, y - 135, x + 50, y - 240, fill="white")
-    c.create_oval(x-130,y-100,x+50,y-165,fill="white")
-    c.create_oval(x+25,y-225,x+75,y-260,fill="white")
-    c.create_oval(x+40,y-235,x+65,y-255,fill="pink")
-    c.create_oval(x+55,y-240,x+65,y-250,fill="black")
-    c.create_oval(x+45,y-245,x+60,y-250,fill="white")
+    legs(x-32*s*o,y)
+    legs(x-55*s*o,y-15*s)
+    legs(x-17*s*o,y-15*s)
+    c.create_oval(x + 10*s*o, y - 67*s, x + 25*s*o, y - 120*s, fill="white")
+    c.create_oval(x-65*s*o,y-50*s,x+25*s*o,y-82*s,fill="white")
+    c.create_oval(x+12*s*o,y-112*s,x+37*s*o,y-130*s,fill="white")
+    c.create_oval(x+20*s*o,y-117*s,x+37*s*o,y-125*s,fill="pink")
+    c.create_oval(x+27*s*o,y-120*s,x+37*s*o,y-125*s,fill="black")
+    c.create_oval(x+22*s*o,y-122*s,x+37*s*o,y-125*s,fill="white")
     brushColor("white")
-    polygon([[x+30,y-235],[x+15,y-250],[x+30,y-245]])
-    polygon([[x+32,y-240],[x+17,y-255],[x+35,y-250]])
+    penColor("Black")
+    polygon([[x+15*s*o,y-117*s],[x+7*s*o,y-125*s],[x+15*s*o,y-122*s]])
+    polygon([[x+16*s*o,y-120*s],[x+8*s*o,y-127*s],[x+17*s*o,y-125*s]])
 
-
-
-lama(200,500)
+lama(110,310,0.5,1)
+lama(170,270,0.5,1)
+lama(190,340,0.5,-1)
+lama(480,480,1.5,-1)
+lama(0,780,2.5,1)
 
 run()
-
-
-
-run()
-
 
 
 
